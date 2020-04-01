9097b8fc08a29f97f3c302f5d969fcd72067d006
firecrow <fire@firecrow.com>
2020-03-31 22:35:23 -0400

    mid refactor

--- app/src/main/java/com/firecrow/windmill/Activity.kt
+++ app/src/main/java/com/firecrow/windmill/Activity.kt
@@ -3,10 +3,16 @@ package com.firecrow.windmill
 import android.content.Context
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
+import android.graphics.*
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.palette.graphics.Palette
 
 class Activity : AppCompatActivity() {
     override fun onCreate(instance: Bundle? ) {
@@ -26,7 +32,52 @@ class Activity : AppCompatActivity() {
     }
 }
 
+fun getRowColor(icon: Drawable): Int {
+    val iconb: Bitmap = Bitmap.createBitmap(10, 10, Bitmap.Config.ARGB_8888)
+    val canvas: Canvas = Canvas(iconb)
+    icon.setBounds(0, 0, 10, 10)
+    icon.draw(canvas)
 
-class WMAdapter(context: Context, resource: Int, list: ArrayList<ApplicationInfo>):ArrayAdapter<ApplicationInfo>(context, resource, list) {
+    val p: Palette = Palette.from(iconb).generate()
+    return p.getVibrantColor(0xff000000.toInt())
+}
+
+fun getIconFilter(icon: Drawable): ColorFilter {
+    val matrix = ColorMatrix()
+    matrix.setSaturation(0.0f)
+
+    // https://stackoverflow.com/questions/30340591/changing-an-imageview-to-black-and-white
+    return ColorMatrixColorFilter(matrix);
+}
+
+class WMAdapter(context: Context, resource: Int, val alist: ArrayList<ApplicationInfo>):ArrayAdapter<ApplicationInfo>(context, resource, alist) {
+    override fun getCount(): Int { return alist.count() }
+    override fun getItem(idx: Int): ApplicationInfo { return alist.get(idx) }
+    override fun getItemId(idx: Int): Long { return idx.toLong() }
+
+
+    override fun getView(idx: Int, view: View, parent: ViewGroup) {
+        return if view == null
+
+        inflator:LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflator
+        val v = layoutInflater.inflate(R.layout.grid_item, null);
+
+        ApplicationInfo data = list.get(idx);
+        return if data == null
+
+        val namev = (TextView) view.findViewById(R.id.name);
+        val iconv = (ImageView) view.findViewById(R.id.icon);
+
+        val icon = data.loadIcon(packageManager)
+
+        val color = getRowColor(data.loadIcon(packageManager));
+
+        namev.setText(data.loadLabel(packageManager));
+        iconv.setImageDrawable(icon);
+        iconv.setColorFilter(getIconFilter();
+        view.setBackgroundColor(color);
+
+    }
+    return view;
 
 }
